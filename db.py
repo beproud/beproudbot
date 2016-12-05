142d435863f71a3f85120fe387bbedd1a427ebf1
wan <nishikawa0228@sj9.so-net.ne.jp>
2016-12-05 12:15:51 +0900

    refs #10 zope.sqlalchemy外す

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 from sqlalchemy import engine_from_config
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from zope.sqlalchemy import ZopeTransactionExtension
 
-Session = sessionmaker()
+
+Session = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 # beproudbotでModelを追加した場合、
