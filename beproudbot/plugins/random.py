d1298aa5f0105fb3a7bea56f3776c28fc5ccbc99
Takanori Suzuki <takanory@beproud.jp>
2016-12-09 13:26:26 +0900

    refs #random コマンドを追加

---
+++ beproudbot/plugins/random.py
@@ -0,0 +1,48 @@
+import random
+
+from slackbot.bot import respond_to
+from slackbot import settings
+import slacker
+
+
+@respond_to('^random$')
+@respond_to('^random\s+(active|help)$')
+def random_command(message, subcommand=None):
+    """
+    チャンネルにいるメンバーからランダムに一人を選んで返す
+    - https://github.com/os/slacker
+    - https://api.slack.com/methods/channels.info
+    - https://api.slack.com/methods/users.getPresence
+    - https://api.slack.com/methods/users.info
+    """
+
+    if subcommand == 'help':
+        message.send('''- `$random`: チャンネルにいるメンバーからランダムに一人を選ぶ
+- `$random active`: チャンネルにいるactiveなメンバーからランダムに一人を選ぶ
+''')
+        return
+
+    # チャンネルのメンバー一覧を取得
+    channel = message.body['channel']
+    webapi = slacker.Slacker(settings.API_TOKEN)
+    cinfo = webapi.channels.info(channel)
+    members = cinfo.body['channel']['members']
+
+    # bot の id は除く
+    bot_id = message._client.login_data['self']['id']
+    members.remove(bot_id)
+
+    member_id = None
+    while not member_id:
+        # メンバー一覧からランダムに選んで返す
+        member_id = random.choice(members)
+        if subcommand == 'active':
+            # active が指定されている場合は presence を確認する
+            presence = webapi.users.get_presence(member_id)
+            if presence.body['presence'] == 'away':
+                members.remove(member_id)
+                member_id = None
+
+    user_info = webapi.users.info(member_id)
+    name = user_info.body['user']['name']
+    message.send('{} さん、君に決めた！'.format(name))
