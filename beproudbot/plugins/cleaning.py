25e9259300a933a3e9abaa399882e5c37f634f6b
wan <nishikawa0228@sj9.so-net.ne.jp>
2017-02-06 09:26:50 +0900

    refs #6 README追記漏れ修正

--- beproudbot/plugins/cleaning.py
+++ beproudbot/plugins/cleaning.py
@@ -17,7 +17,7 @@ HELP = """
 - `$cleaning add <user_name> <day_of_week>`: 掃除当番を追加する
 - `$cleaning del <user_name> <day_of_week>`: 掃除当番から削除する
 - `$cleaning swap <user_name> <user_name>`: 掃除当番を入れ替える
-- `$cleaning move <user_name> <day_of_week>`: 掃除当番の曜日を入れ替える
+- `$cleaning move <user_name> <day_of_week>`: 掃除当番の曜日を移動する
 - `$cleaning help`: cleaningコマンドの使い方を返す
 - ※<day_of_week> は月、火、水、木、金が指定可能です
 """
