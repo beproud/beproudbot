6837d883bf495eac34b7fdf31608889e6b515114
wan <nishikawa0228@sj9.so-net.ne.jp>
2017-02-16 01:07:59 +0900

    refs #5 WIPコード修正

--- beproudbot/plugins/kudo.py
+++ beproudbot/plugins/kudo.py
@@ -5,43 +5,41 @@ from db import Session
 from beproudbot.plugins.kudo_models import KudoHistory
 
 HELP = """
-- `$<name>++`: nameに++を行う
-- `$<name>--`: nameに--を行う
-- `$kudo rank <name>`: nameの
-- `$kudo rank_to <name>`: nameが++/--した対象を一覧表示する
-- `$kudo rank_from <name>`: nameが++/--された対象を一覧表示する
-- `$kudo monthly <name> [month]`:
+- `$<name>++`: 指定された名前に対して +1 カウントします
+- `$<name>--`: 指定された名前に対して -1 カウントします
+- `$kudo rank_to <name>`: nameが++/--した対象をranking形式で表示する
+- `$kudo rank_from <name>`: nameが++/--された対象をranking形式表示する
 - `$kudo help`: kudoコマンドの使い方を返す
 """
 
 
-@respond_to('^kudo\s+++$')
-def kudo_plusplus(message):
-    pass
+@respond_to('^(\S*[^\+|\-])\s*(\+\+|\-\-)$')
+def kudo(message, name, action):
+    """ 指定された名前に対して ++ または -- する
 
+    OK:
+       $name++、$name ++、$name  ++
+    NG:
+       $name---、$name ---、$name-++
 
-@respond_to('^kudo\s+--$')
-def kudo_minusminus(message):
-    pass
-
-
-@respond_to('^kudo\s+rank\s+(\S+)$')
-def show_kudo_rank(message, name):
+    :param message: slackbot.dispatcher.Message
+    :param name str: ++ または -- をする対象の名前
+    :param action str: ++を指定した場合+1、--を指定した場合-1
+    """
     pass
 
 
 @respond_to('^kudo\s+rank_to\s+(\S+)$')
 def show_kudo_rank_to(message, name):
+    """nameが++/--した対象をRanking形式で表示する
+    """
     pass
 
 
 @respond_to('^kudo\s+rank_from\s+(\S+)$')
 def show_kudo_rank_from(message, name):
-    pass
-
-
-@respond_to('^kudo\s+monthly\s+(\S+)\s+(\S+)$')
-def show_kudo_monthly_point(message, name):
+    """nameが++/--された対象をRanking形式で表示する
+    """
     pass
 
 
