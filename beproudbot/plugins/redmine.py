e57c90c26056636a6ce38bcbfba6b9a627d0c19b
James Van Dyne <james.van.dyne@beproud.jp>
2017-08-15 15:49:44 +0900

    Restricts responses to allow channels for the project Adds logging for when we don't have access to a project (403) Adds logging for when projects can't respond in that channel Fixes flake8 issues

--- beproudbot/plugins/redmine.py
+++ beproudbot/plugins/redmine.py
@@ -1,18 +1,19 @@
+"""redmineのチケット情報を参照."""
 import requests
+
+import logging
+
 from slackbot.bot import listen_to
-
-
 from db import Session
 from beproudbot.plugins.redmine_models import RedmineUser, ProjectRoom
 
-HELP = '''
 
-'''
+logging = logging.getLogger(__name__)
 
 
 @listen_to('[t#](\d+)')
 def show_ticket_information(message, ticket_id):
-    """Show redmine ticket information in Slack
+    """Redmineのチケット情報を参照する.
 
     :param message: slackbotの各種パラメータを保持したclass
     :param ticket_id ticket id
@@ -30,13 +31,18 @@ def show_ticket_information(message, ticket_id):
         url = "https://project.beproud.jp/redmine/issues/{}".format(ticket_id)
         headers = {'X-Redmine-API-Key': user.api_key}
         res = requests.get("{}.json".format(url), headers=headers)
-        ticket = res.json()
+        if res.status_code == 200:
+            ticket = res.json()
 
-        # proj_room = s.query(ProjectRoom).\
-        #   filter(ProjectRoom.id == ticket["issue"]["project"]["id"]).first()
-        #if proj_room and source_channel in proj_room:
-        message.send("{} {}".format(ticket["issue"]["subject"], url))
+            proj_room = s.query(ProjectRoom).\
+                filter(ProjectRoom.id == ticket["issue"]["project"]["id"]).first()
+
+            if proj_room and source_channel in proj_room.rooms.split(","):
+                message.send("{} {}".format(ticket["issue"]["subject"], url))
+            else:
+                logging.debug("{} doesn't have permissions to show in {}".format(ticket_id,
+                                                                                 source_channel))
+        else:
+            logging.info("{} doesn't have access to ticket #{}".format(source_user, ticket_id))
     else:
         message.send('{}は登録されていません。'.format(source_user))
-
-
--- beproudbot/plugins/redmine_models.py
+++ beproudbot/plugins/redmine_models.py
@@ -1,11 +1,11 @@
+"""redmineのモデル."""
 from sqlalchemy import Column, Integer, Unicode
 from db import Base
 
 
 class RedmineUser(Base):
-    """
-    Slack username and Redmine API key map 
-    
+    """SlackのユーザとRedmineのAPI Keyのmap.
+
     :param Base: `sqlalchemy.ext.declarative.api.DeclarativeMeta` を
         継承したclass
     """
@@ -14,18 +14,20 @@ class RedmineUser(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Unicode(100), nullable=False)  # Slack username
-    api_key = Column(Unicode(100), nullable=False)
+    api_key = Column(Unicode(100), nullable=False)  # Redmine API Key
 
 
 class ProjectRoom(Base):
-    """
+    """Projectはどのチャネルで返してもよいのmap.
+
     id: Redmine project id
     rooms: CSV room list i.e. this project's tickets can only be replied to in these rooms
-    
+
     :param Base: `sqlalchemy.ext.declarative.api.DeclarativeMeta` を
         継承したclass
     """
+
     __tablename__ = "remine_projectroom"
 
-    id = Column(Integer, primary_key=True)
-    rooms = Column(Unicode(255), nullable=False)   # csv
+    id = Column(Integer, primary_key=True)  # Redmineのprojectのid
+    rooms = Column(Unicode(255), nullable=False)  # "slack-api-practice,bp-kaizen"
