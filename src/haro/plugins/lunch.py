55b6bdf44a6c8590827c223b4fbda8b9bff28e73
altnight <altnight@beproud.jp>
2018-01-11 15:56:41 +0900

    fix flake8

--- src/haro/plugins/lunch.py
+++ src/haro/plugins/lunch.py
@@ -6,8 +6,8 @@ import kml2geojson as k2g
 from geopy.distance import vincenty
 from slackbot.bot import respond_to
 
-KML_SOURCE = 'https://www.google.com/maps/d/u/0/kml?hl=en&mid=1J4U-QXOe1Zi_4Lw5UxaL8AriG6M&lid=zubJL41y6fLI.k8KrINTXzJI4&forcekml=1'
-MAPS_URL_BASE = 'http://maps.google.com/maps/ms?ie=UTF&msa=0&msid=101243938118389113627.00047d5491d28f02d4f57&z=19&iwloc=A&ll='
+KML_SOURCE = 'https://www.google.com/maps/d/u/0/kml?hl=en&mid=1J4U-QXOe1Zi_4Lw5UxaL8AriG6M&lid=zubJL41y6fLI.k8KrINTXzJI4&forcekml=1'  # NOQA
+MAPS_URL_BASE = 'http://maps.google.com/maps/ms?ie=UTF&msa=0&msid=101243938118389113627.00047d5491d28f02d4f57&z=19&iwloc=A&ll='  # NOQA
 BP_COORDINATES = (35.68641, 139.70343)
 
 HELP = '''
@@ -43,7 +43,8 @@ def get_distance_from_office(destination):
     :return: 距離（メートル単位）
     """
     office_coordinates = BP_COORDINATES
-    destination_coordinates = (destination['geometry']['coordinates'][1], destination['geometry']['coordinates'][0])
+    destination_coordinates = (destination['geometry']['coordinates'][1],
+                               destination['geometry']['coordinates'][0])
 
     distance = vincenty(office_coordinates, destination_coordinates).meters
 
