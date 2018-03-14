f47b53e8f4a6434e49d8e3e0062c1851cbb1ba21
checkroth <checkroth@gmail.com>
2018-03-14 15:10:26 +0900

    missing trackers

--- tests/test_thx.py
+++ tests/test_thx.py
@@ -6,7 +6,7 @@ from haro.plugins.thx_models import ThxHistory
 
 @pytest.fixture
 def thx_histories():
-    histories = thx.ThxHistoryFactory.create_batch(1)
+    histories = thx.ThxHistoryFactory.create_batch(100)
     return histories
 
 
