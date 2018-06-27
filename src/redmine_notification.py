27f75d482ff01bb807f7f2ad0d294ccecc057a5c
dai kikuchi <kikuchi.dai@gmail.com>
2018-06-27 09:13:15 +0900

    コードの修正

--- src/redmine_notification.py
+++ src/redmine_notification.py
@@ -4,8 +4,7 @@ from db import Session
 from haro.plugins.redmine_models import ProjectChannel
 
 from slackbot_settings import REDMINE_URL
-
-API_KEY = 'de773d446c4b9a0fc41fb2001b9780f8610b62f1';
+from slackbot_settings import API_KEY
 
 def get_ticket_information():
     redmine = Redmine(REDMINE_URL, key=API_KEY)
@@ -19,8 +18,7 @@ def get_ticket_information():
         print ('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
         # プロジェクトのroomを取得
         proj_id = issue.project.id
-        proj_room = s.query(ProjectChannel).filter(ProjectChannel.project_id == proj_id) \
-                .one_or_none()
+        proj_room = s.query(ProjectChannel).filter(ProjectChannel.project_id == proj_id).one_or_none()
         print(proj_room)
 
 def main():
