7249489fd72cb342699acbf0b35a92215f2560c5
dai kikuchi <kikuchi.dai@gmail.com>
2018-06-27 16:40:23 +0900

    ファイルの内容を少し修正

--- src/redmine_notification.py
+++ src/redmine_notification.py
@@ -1,12 +1,29 @@
+"""
+    スクリプト概要
+    ~~~~~~~~~~
+
+    systemdでtimerを作成し、定期的に実行するPython
+    1,Redmineから期限切れ、期限切れそうなチケットを取得する
+    2,チケットの期限によって分類
+    3,各チャンネルに期限切れ、期限が切れそうなチケットの情報を通知する。
+
+"""
+
 from redminelib import Redmine
-from db import Session
 
 from haro.plugins.redmine_models import ProjectChannel
 
-from slackbot_settings import REDMINE_URL
-from slackbot_settings import API_KEY
+from slackbot_settings import REDMINE_URL, API_KEY
+from slackbot.bot import listen_to, respond_to
 
-def get_ticket_information():
+from haro.botmessage import botsend
+from db import Session
+
+@respond_to('^redmine\s+test_daik$')
+def get_ticket_information(message):
+    """Redmineのチケット情報とチケットと結びついているSlackチャンネルを取得
+
+    """
     redmine = Redmine(REDMINE_URL, key=API_KEY)
     # すべてのチケットを取得
     issues = redmine.issue.all(sort='subject:desc')
@@ -15,14 +32,13 @@ def get_ticket_information():
         # due_date属性とdue_dateがnoneの場合は除外
         if not getattr(issue, 'due_date', None):
             continue
-        print ('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
+        print('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
+        botsend('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
         # プロジェクトのroomを取得
         proj_id = issue.project.id
         proj_room = s.query(ProjectChannel).filter(ProjectChannel.project_id == proj_id).one_or_none()
-        print(proj_room)
+        botsend(message, proj_room)
 
-def main():
+
     get_ticket_information()
 
-if __name__ == '__main__':
-    main()
