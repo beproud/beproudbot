b68965504f9ff83b7d05dcf28ab95011aa32bb71
dai kikuchi <kikuchi.dai@gmail.com>
2018-06-29 15:44:02 +0900

    スクリプト全体像の修正

--- src/redmine_notification.py
+++ src/redmine_notification.py
@@ -28,44 +28,63 @@ def get_ticket_information(message):
     # すべてのチケットを取得
     issues = redmine.issue.all(sort='subject:desc')
     s = Session()
+    projects_past_due_date ={}
+    projects_close_to_due_date = {}
     for issue in issues:
         # due_date属性とdue_dateがnoneの場合は除外
         if not getattr(issue, 'due_date', None):
             continue
-        print('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
-        botsend('%d:%s:%s:%s' % (issue.id, issue.subject, issue.author, issue.due_date))
         # プロジェクトのroomを取得
         proj_id = issue.project.id
-        print(proj_id)
+
+        # TODO: redmineと関連付けられたSlackチャンネルがない場合の処理
+        # TODO: チケットごとに1query走るのは無駄だから、下記以外の方法でprojectのroomを取得する
+        # TODO: 1つのredmineプロジェクトが複数のslackチャンネルに関連付けられている場合
+
         proj_room = s.query(ProjectChannel).filter(ProjectChannel.project_id == proj_id).one_or_none()
-        print(proj_room)
 
-        # 各issueのチェンネルを取得する。方法はredmine.pyのshow_ticket_informationのチャンネル取得方法を参照。
+        # TODO: 各issueのチェンネルを取得する。方法はredmine.pyのshow_ticket_informationのチャンネル取得方法を参照。
 
-        # if True: due_dateがあるIssueを、リストの形で期限切れチケットと期限が切れそうなチケットを分別するFunctionに引き渡す。
-            # text = str(issue.id) +'タイトル: '+ str(issue.subject) + ' 責任者: '+ str(issue.author) + 'チケットの期限が過ぎてます。'+ str(issue.due_date)
-            # sc.chat.post_message()メソッドを利用して各チャンネルに期限切れチケット情報を送付
+        if check_past_due_date(True): # due_dateがあるIssueを、リストの形で期限切れチケットと期限が切れそうなチケットを分別するFunctionに引き渡す。
+            # groupを利用し、同じプロジェクトのidでグループ化する。
+            # group issue projects_past_due_date[proj_id]
 
+        elif check_close_to_due_date(True): #  期限が切れそうなチケットをチェックするFunctionを実行
+            # groupを利用し、同じプロジェクトのidでグループ化する。
+            # group issue projects_close_to_due_date[proj_id]
 
-            # else :Falseの場合
-               # if True: 期限が切れそうなチケットをチェックするファンクションを実行
-                        #     text = str(issue.id) +'タイトル: '+ str(issue.subject) + ' 責任者: '+ str(issue.author) + 'チケットの期限が切れそうです。'+ str(issue.due_date)
-                        # 　  sc.chat.post_message()メソッドを使って、チケット情報を各チャンネルに送付
-        　
-                # else: 期限に余裕があるチケット
-        　　　　　　　# continue
-        　　　　　　
-"""期限切れたチケットをチェックするFunction作成
-   期限が切れていたらTrueを返す。それ以外はFalse
+        else: #期限に余裕があるチケット
+            continue
+
+    for project in projects_past_due_date:
+        #redmineプロジェクト単位で期限が過ぎたチケットをまとめて通知
+        # TODO: 期限切れのチケットカウント数取得
+        # (message, '期限が切れたチケットは{}件です'.format('、'.join(projects_past_due_date[project])))
+    for project in projects_close_to_due_date:
+        # redmineプロジェクト単位で期限が過ぎそうなチケットをまとめて通知
+        # TODO: 期限切れのチケットカウント数取得
+        # (message, 'もうすぐ期限切れチケットは{}件です'.format('、'.join( each projects_close_to_due_date[project])))
+        return True
+
+def check_past_due_date(True):
+    """期限切れたチケットをチェックするFunction作成
+    期限が切れていたらTrueを返す。それ以外はFalse
+
+    :param tickets: redmineとslackチャンネルのデータを持ったlist
+
+    """
+
+    return True
+
+def check_close_to_due_date(True):
+    """期限切れたチケットをチェックするFunction作成
+    期限が切れていたらTrueを返す。それ以外はFalse
+
+    :param tickets: redmineとslackチャンネルのデータを持ったlist
+
+    """
+
+    return True
    
  
-　　:param tickets: redmineとslackチャンネルのデータを持ったlist
-    
-"""
 
-"""期限切そうなチケットをチェックするFunction
-   期限が切れそうだったらTrueを返す。それ以外はFalse
-
-
-　　:param tickets: redmineとslackチャンネルのデータを持ったlist
-"""
