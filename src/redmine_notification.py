772e9b8541b4fb9ee8d98a6dd37717759c145a70
dai kikuchi <kikuchi.dai@gmail.com>
2018-07-04 12:00:12 +0900

    レビューを途中まで修正、単独のスクリプトとして動くように変更

--- src/redmine_notification.py
+++ src/redmine_notification.py
@@ -1,29 +1,37 @@
 """
-    スクリプト概要
-    ~~~~~~~~~~
-
-    systemdでtimerを作成し、定期的に実行するPython
-    1,Redmineから期限切れ、期限切れそうなチケットを取得する
-    2,チケットの期限によって分類
-    3,各チャンネルに期限切れ、期限が切れそうなチケットの情報を通知する。
-
+スクリプト概要
+~~~~~~~~~~
+systemdでtimerを作成し、定期的に実行するPython
+1,Redmineから期限切れ、期限切れそうなチケットを取得する
+2,チケットの期限によって分類
+3,各チャンネルに期限切れ、期限が切れそうなチケットの情報を通知する。
 """
 
 from redminelib import Redmine
-from datetime import timedelta, datetime
+from datetime import timedelta, date
 from slackclient import SlackClient
+import argparse
+from textwrap import dedent
+from configparser import ConfigParser, NoSectionError
 
 from haro.plugins.redmine_models import ProjectChannel
 
-from slackbot_settings import REDMINE_URL, API_KEY
-from slackbot.bot import listen_to, respond_to
+from slackbot_settings import REDMINE_URL, API_KEY, SLACK_API_TOKEN
+from configparser import ConfigParser, NoSectionError
+
+from db import init_dbsession
+from slackbot_settings import (
+    SQLALCHEMY_URL,
+    SQLALCHEMY_ECHO,
+    SQLALCHEMY_POOL_SIZE,
+)
 
 from db import Session
 
-@respond_to('^redmine\s+test_daik$')
+
+# @respond_to('^redmine\s+test_daik$')
 def get_ticket_information():
     """Redmineのチケット情報とチケットと結びついているSlackチャンネルを取得
-
     """
     redmine = Redmine(REDMINE_URL, key=API_KEY)
     # すべてのチケットを取得
@@ -39,7 +47,7 @@ def get_ticket_information():
         # issueのデータをSlack通知用にformatする。
         issue_display = display_issue(issue)
         # issueの期限が過ぎていた場合
-       if check_past_due_date(issue.due_date):
+        if is_past_due_date(issue.due_date):
            # proj_idをkeyにして値にformatしたIssue情報の値を入れる。
            # 辞書のkeyと値の例:{proj_id: ['- 2017-03-31 23872: サーバーセキュリティーの基準を作ろう(@takanory)'], xxx:['- xxxxxxx']}
             if proj_id not in projects_past_due_date.keys():
@@ -47,49 +55,51 @@ def get_ticket_information():
             else:
                 projects_past_due_date[proj_id].append(issue_display)
         # issueの期限が1週間以内の場合
-        elif check_close_to_due_date(issue.due_date):
+        elif is_close_to_due_date(issue.due_date):
             if proj_id not in projects_close_to_due_date.keys():
                  projects_close_to_due_date[proj_id] = [issue_display]
             else:
-                projects_close_to_due_date[proj_id].append(issue_display)
+                 projects_close_to_due_date[proj_id].append(issue_display)
         else:
             continue
 
     # 各プロジェクトのチケット通知をSlackチャンネルに送る。
     send_ticket_info_to_channels(projects_past_due_date, 0)
+    print(projects_past_due_date)
     send_ticket_info_to_channels(projects_close_to_due_date, 1)
+
 
 def display_issue(issue):
     """issueの詳細をSlackに表示用にフォーマットする。
 
-    　 :param issue: redmineのissue
+    フォーマット例:
+    - 2017-03-31 23872: サーバーセキュリティーの基準を作ろう(@takanory)
+    :param issue: redmineのissue
     """
 
-    return '- ' + str(issue.due_date) + ' ' + str(issue.id) + ': ' + str(issue.subject) + '(@' + str(issue.author) + ')'
+    return '{} {} {}: {} (@{})'.format('-', issue.due_date, issue.id, issue.subject, issue.author)
 
-def check_past_due_date(due_date):
+
+def is_past_due_date(due_date):
     """期限切れたチケットをチェックするFunction
     期限が切れていたらTrueを返す。それ以外はFalse
 
     :param due_date: 各チケットのdue_date
     """
-    today = datetime.now()
-    if datetime.combine(due_date, datetime.min.time()) < today:
-        return True
-    else:
-        return False
+    today = date.today()
 
-def check_close_to_due_date(due_date):
+    return due_date < today
+
+
+def is_close_to_due_date(due_date):
     """期限が1週間以内に切れそうなチケットをチェックするFunction
     期限が切れていたらTrueを返す。それ以外はFalse
 
     :param due_date: 各チケットのdue_date
     """
-    today = datetime.now()
-    if datetime.combine(due_date, datetime.min.time()) < today - timedelta(7):
-        return True
-    else:
-        return False
+    today = date.today()
+
+    return due_date < today - timedelta(7)
 
 
 def send_ticket_info_to_channels(projects, type):
@@ -103,7 +113,18 @@ def send_ticket_info_to_channels(projects, type):
     for project in projects.keys():
         # 各プロジェクトのproj_roomを獲得する。
         proj_room = s.query(ProjectChannel).filter(ProjectChannel.project_id == project).one_or_none()
-
+        # for testing only
+        issue_count = len(projects[project])
+        if not type:  # 期限切れチケット
+            message = '期限が切れたチケットは' + str(issue_count) + ' 件です\n'
+        else:  # 期限切れそうなチケット
+            message = 'もうすぐ期限が切れそうなチケットは' + str(issue_count) + ' 件です\n'
+        message += '\n'.join(projects[project])
+        sc.api_call(
+            "chat.postMessage",
+            channel="CBEL53NPM",
+            text=message
+        )
         # api_call()を使用し、すべてのSlackチャンネルに期限が切れたチケット、期限が切れそうな通知をチケットまとめて送る
         if proj_room:
             # 1つのredmineプロジェクトが複数のslackチャンネルに関連付けられているケースに対応
@@ -115,9 +136,52 @@ def send_ticket_info_to_channels(projects, type):
             else: # 期限切れそうなチケット
                 message = 'もうすぐ期限が切れそうなチケットは' + str(issue_count) + ' 件です\n'
                 message += '\n'.join(projects[project])
-            for channel in channels:
-                sc.api_call(
-                    "chat.postMessage",
-                    channel=channel,
-                    text= message
-                )
+            # for channel in channels:
+            #     sc.api_call(
+            #         "chat.postMessage",
+            #         channel=channel,
+            #         text= message
+            #     )
+
+def get_argparser():
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=dedent('''\
+            説明:
+            haroの設定ファイルを読み込んだ後にslackbotを起動します'''))
+
+    parser.add_argument('-c', '--config',
+                        type=argparse.FileType('r'),
+                        default='alembic/conf.ini',
+                        help='ini形式のファイルをファイルパスで指定します')
+
+    return parser
+
+def main():
+    """設定ファイルをparseして、slackbotを起動します
+
+    1. configparserで設定ファイルを読み込む
+    2. 設定ファイルに `alembic` セクションが設定されているかチェック
+    3. 設定ファイルの情報でDB周りの設定を初期化
+    4. slackbotの処理を開始
+    """
+    parser = get_argparser()
+    args = parser.parse_args()
+    conf = ConfigParser()
+    conf.read_file(args.config)
+    # 環境変数で指定したいため ini ファイルでなくここで追記
+    conf["alembic"]['sqlalchemy.url'] = SQLALCHEMY_URL
+    conf["alembic"]['sqlalchemy.echo'] = SQLALCHEMY_ECHO
+    # if SQLALCHEMY_POOL_SIZE:
+    #     conf["alembic"]['sqlalchemy.pool_size'] = '20' #SQLALCHEMY_POOL_SIZE
+    if not conf.has_section('alembic'):
+        raise NoSectionError('alembic')
+
+    init_dbsession(conf['alembic'])
+    get_ticket_information()
+
+
+if __name__ == "__main__":
+    main()
+
