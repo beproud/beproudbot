674e5f9a8aaf1bf07422039139fb16ebf082df90
altnight <altnight@beproud.jp>
2018-09-27 15:54:56 +0900

    ざつに例外処理

--- src/haro/plugins/amesh.py
+++ src/haro/plugins/amesh.py
@@ -1,3 +1,4 @@
+import logging
 import io
 import tempfile
 from datetime import datetime
@@ -7,9 +8,13 @@ import requests
 from PIL import Image
 from slackbot.bot import respond_to
 
+from haro.botmessage import botsend
+
 HELP = """
 amesh(http://tokyo-ame.jwa.or.jp/)の天気を表示したい
 """
+
+logger = logging.getLogger(__name__)
 
 
 @contextmanager
@@ -35,31 +40,35 @@ def amesh(message):
     yyyymmddhh = n.strftime("%Y%m%d%H")
     mm = "{:02d}".format(n.minute // 5 * 5)
 
-    # 画像の合成
-    # 000 はエリアごとの固定値で050,100,150があるけど決め打ちで
-    with _get_image("http://tokyo-ame.jwa.or.jp/map/msk000.png") as image_msk, \
-        _get_image("http://tokyo-ame.jwa.or.jp/map/map000.jpg") as image_map, \
-        _get_image("http://tokyo-ame.jwa.or.jp/mesh/000/{}{}.gif".format(
-            yyyymmddhh, mm)) as image_weather:
-        merged = Image.alpha_composite(image_map, image_weather)
-        merged2 = Image.alpha_composite(merged, image_msk)
+    try:
+        # 画像の合成
+        # 000 はエリアごとの固定値で050,100,150があるけど決め打ちで
+        with _get_image("http://tokyo-ame.jwa.or.jp/map/msk000.png") as image_msk, \
+            _get_image("http://tokyo-ame.jwa.or.jp/map/map000.jpg") as image_map, \
+            _get_image("http://tokyo-ame.jwa.or.jp/mesh/000/{}{}.gif".format(
+                yyyymmddhh, mm)) as image_weather:
+            merged = Image.alpha_composite(image_map, image_weather)
+            merged2 = Image.alpha_composite(merged, image_msk)
 
-    # slack にアップロードするために一時的にtmpfileに書き出す
-    with tempfile.NamedTemporaryFile() as tmp:
-        name = "{}{}.png".format(yyyymmddhh, mm)
-        tmpname = "{}.png".format(tmp.name)
-        merged2.save(tmpname)
+        # slack にアップロードするために一時的にtmpfileに書き出す
+        with tempfile.NamedTemporaryFile() as tmp:
+            name = "{}{}.png".format(yyyymmddhh, mm)
+            tmpname = "{}.png".format(tmp.name)
+            merged2.save(tmpname)
 
-        # せっかくなので天気もみれるようにしてる
-        comment = "時刻: {:%Y年%m月%d日 %H}:{}\n".format(n, mm) + \
-                  "公式: http://tokyo-ame.jwa.or.jp/\n" + \
-                  "渋谷区の天気: https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区"
-        # 外部サイトに投稿してURLを貼る方法(S3とか)だとaccesskey設定等いるのでslackに直接アップロード
-        sc = message._client.webapi
-        sc.files.upload(
-            file_=tmpname,
-            initial_comment=comment,
-            title=name,
-            filename=name,
-            channels=message.channel._body["name"],
-        )
+            # せっかくなので天気もみれるようにしてる
+            comment = "時刻: {:%Y年%m月%d日 %H}:{}\n".format(n, mm) + \
+                      "公式: http://tokyo-ame.jwa.or.jp/\n" + \
+                      "渋谷区の天気: https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区"
+            # 外部サイトに投稿してURLを貼る方法(S3とか)だとaccesskey設定等いるのでslackに直接アップロード
+            sc = message._client.webapi
+            sc.files.upload(
+                file_=tmpname,
+                initial_comment=comment,
+                title=name,
+                filename=name,
+                channels=message.channel._body["name"],
+            )
+    except Exception as e:
+        botsend(message, "エラーなのでまた叩いてくだし")
+        logger.exception("amesh exception")
