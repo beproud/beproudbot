de38dab65587dcddfa4051a610bf648647ca14ae
altnight <altnight@beproud.jp>
2018-09-27 15:47:07 +0900

    レビュー対応

--- src/haro/plugins/amesh.py
+++ src/haro/plugins/amesh.py
@@ -29,13 +29,11 @@ def _get_image(url):
 def amesh(message):
 
     # ameshでは5分ごとにデータが作成されるため、桁を揃えてからリクエストしたい
+    # 同じ画像をリクエストすることありそうなのでキャッシュいれたほうがよさそうだけど、
+    # 現状不便ないので特に対応をいれていない
     n = datetime.now()
     yyyymmddhh = n.strftime("%Y%m%d%H")
-    mm = n.strftime("%M")
-    if mm[1] in ["0", "1", "2", "3", "4"]:
-        mm = "{}0".format(mm[0])
-    else:
-        mm = "{}5".format(mm[0])
+    mm = "{:02d}".format(n.minute // 5 * 5))
 
     # 画像の合成
     # 000 はエリアごとの固定値で050,100,150があるけど決め打ちで
@@ -53,9 +51,9 @@ def amesh(message):
         merged2.save(tmpname)
 
         # せっかくなので天気もみれるようにしてる
-        comment = "時刻: {}:{}\n".format(n.strftime("%Y年%m月%m日 %H"), mm) + \
+        comment = "時刻: {:%Y年%m月%d日 %H}:{}\n".format(n, mm) + \
                   "公式: http://tokyo-ame.jwa.or.jp/\n" + \
-                  "天気: https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区"
+                  "<渋谷区の天気|https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区>"
         # 外部サイトに投稿してURLを貼る方法(S3とか)だとaccesskey設定等いるのでslackに直接アップロード
         sc = message._client.webapi
         sc.files.upload(
