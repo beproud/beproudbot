49b21fd3bb27ce6dea4be988ef73cac3ec8bcb3d
James Van Dyne <james.van.dyne@beproud.jp>
2019-04-26 15:49:00 +0900

    - Adds ability to show a timeline

--- src/haro/plugins/emergency.py
+++ src/haro/plugins/emergency.py
@@ -8,6 +8,7 @@ from slackbot import settings
 from db import Session
 from haro.botmessage import botsend
 from haro.plugins.emergency_models import Timeline, TimelineEntry
+from haro.slack import get_user_display_name
 
 
 NO_ACTIVE_EMERGENCY = '緊急タスクを監視されていません。'
@@ -16,10 +17,19 @@ ADDED_TO_TIMELINE = '「{}」に追加しました。'
 TIMELINE_START = '「{}」という緊急タスクを監視し始めます。'
 TIMELINE_END = '「{}」を終了しました。'
 
+MARKDOWN_TEMPLATE = """# {}
+
+タイムライン
+
+{}
+"""
+
 HELP = """
 - `$emergency start <タイトル>`: コマンドを実行したSlackチャンネルに緊急タスク監視ボットを開始される
-- `$emergency update <進捗>`: コマンドを実行したSlackチャンネルに緊急タスクのタイムラインに進捗メッセージを追加する
-- `$emergency end`: コマンドを実行したSlackチャンネルに緊急タスク監視ボットを終了される
+- `$emergency update <進捗>`: 監視中緊急タスクのタイムラインに進捗メッセージを追加する
+- `$emergency end`: 緊急タスク監視を終了する
+- `$emergency list`: コマンドを実行したSlackチャンネルに監視された緊急タスクを一覧に表示をする
+- `$emergency timeline <timeline_id>`: 指定したタイムラインをmarkdownで表示される
 
 - `$emergency help`: emergencyのコマンドの使い方を返す
 """
@@ -144,3 +154,38 @@ def list_emergencies(message):
                   files={'file': ("%s_emergencies.csv" % channel_name, output.getvalue(),
                                   'text/csv', )})
 
+
+@respond_to('^emergency\s+timeline\s+(\S+)$')
+def show_timeline(message, timeline_id):
+    s = Session()
+
+    channel_id = message.channel._body['id']
+    channel_name = message.channel._body['name']
+    timeline = (s.query(Timeline)
+                .filter(Timeline.room == channel_id)
+                .filter(Timeline.id == timeline_id)).one_or_none()
+
+    if not timeline:
+        return
+
+    entries = (s.query(TimelineEntry)
+                 .filter(TimelineEntry.timeline_id == timeline.id)
+                 .order_by(TimelineEntry.ctime))
+
+    rows = ['- {} {} {}'.format(
+                entry.ctime.strftime("%Y/%m/%d %H:%M"),
+                entry.entry,
+                get_user_display_name(entry.created_by))
+            for entry in entries]
+
+    contents = MARKDOWN_TEMPLATE.format(timeline.title, '\n'.join(rows))
+
+    param = {
+        'token': settings.API_TOKEN,
+        'channels': channel_id,
+        'title': '{}のタイムライン'.format(timeline.title)
+    }
+    requests.post(settings.FILE_UPLOAD_URL,
+                  params=param,
+                  files={'file': ("%s_timeline.md" % channel_name, contents,
+                                  'text/markdown', )})
