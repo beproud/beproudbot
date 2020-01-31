be8b0802c2c0911675394d445dac3a2f163a9a15
altnight <altnight@beproud.jp>
2020-01-31 10:59:01 +0900

    $amesh retry 機能追加

--- src/haro/plugins/amesh.py
+++ src/haro/plugins/amesh.py
@@ -1,4 +1,5 @@
 import logging
+import time
 import io
 import tempfile
 from datetime import datetime
@@ -40,35 +41,48 @@ def amesh(message):
     yyyymmddhh = n.strftime("%Y%m%d%H")
     mm = "{:02d}".format(n.minute // 5 * 5)
 
-    try:
-        # 画像の合成
-        # 000 はエリアごとの固定値で050,100,150があるけど決め打ちで
-        with _get_image("http://tokyo-ame.jwa.or.jp/map/msk000.png") as image_msk, \
-            _get_image("http://tokyo-ame.jwa.or.jp/map/map000.jpg") as image_map, \
-            _get_image("http://tokyo-ame.jwa.or.jp/mesh/000/{}{}.gif".format(
-                yyyymmddhh, mm)) as image_weather:
-            merged = Image.alpha_composite(image_map, image_weather)
-            merged2 = Image.alpha_composite(merged, image_msk)
+    # 雑リトライ
+    MAX_RETRY_NUM = 3
+    for i in range(1, MAX_RETRY_NUM+1):
 
-        # slack にアップロードするために一時的にtmpfileに書き出す
-        with tempfile.NamedTemporaryFile() as tmp:
-            name = "{}{}.png".format(yyyymmddhh, mm)
-            tmpname = "{}.png".format(tmp.name)
-            merged2.save(tmpname)
+        logger.info("amesh:: trying ... {}/{}".format(i, MAX_RETRY_NUM))
 
-            # せっかくなので天気もみれるようにしてる
-            comment = "時刻: {:%Y年%m月%d日 %H}:{}\n".format(n, mm) + \
-                      "公式: http://tokyo-ame.jwa.or.jp/\n" + \
-                      "渋谷区の天気: https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区"
-            # 外部サイトに投稿してURLを貼る方法(S3とか)だとaccesskey設定等いるのでslackに直接アップロード
-            sc = message._client.webapi
-            sc.files.upload(
-                file_=tmpname,
-                initial_comment=comment,
-                title=name,
-                filename=name,
-                channels=message.channel._body["name"],
-            )
-    except Exception as e:
-        botsend(message, "エラーなのでまた叩いてくだし")
-        logger.exception("amesh exception")
+        try:
+            # 画像の合成
+            # 000 はエリアごとの固定値で050,100,150があるけど決め打ちで
+            with _get_image("http://tokyo-ame.jwa.or.jp/map/msk000.png") as image_msk, \
+                _get_image("http://tokyo-ame.jwa.or.jp/map/map000.jpg") as image_map, \
+                _get_image("http://tokyo-ame.jwa.or.jp/mesh/000/{}{}.gif".format(
+                    yyyymmddhh, mm)) as image_weather:
+                merged = Image.alpha_composite(image_map, image_weather)
+                merged2 = Image.alpha_composite(merged, image_msk)
+
+            # slack にアップロードするために一時的にtmpfileに書き出す
+            with tempfile.NamedTemporaryFile() as tmp:
+                name = "{}{}.png".format(yyyymmddhh, mm)
+                tmpname = "{}.png".format(tmp.name)
+                merged2.save(tmpname)
+
+                # せっかくなので天気もみれるようにしてる
+                comment = "時刻: {:%Y年%m月%d日 %H}:{}\n".format(n, mm) + \
+                          "公式: http://tokyo-ame.jwa.or.jp/\n" + \
+                          "渋谷区の天気: https://weathernews.jp/onebox/35.679311/139.710717/temp=c&q=東京都渋谷区"
+                # 外部サイトに投稿してURLを貼る方法(S3とか)だとaccesskey設定等いるのでslackに直接アップロード
+                sc = message._client.webapi
+                sc.files.upload(
+                    file_=tmpname,
+                    initial_comment=comment,
+                    title=name,
+                    filename=name,
+                    channels=message.channel._body["name"],
+                )
+        except Exception as e:
+            logger.exception("amesh exception")
+            if i == MAX_RETRY_NUM:
+                botsend(message, "{}回目: だめでした…また動かしてみてね".format(i))
+            else:
+                botsend(message, "{}回目: だめでした…ちょっと待ちます".format(i))
+                # 特定の分数(5で割れる分?)だと取得できないことが多いので、30秒ずつ空けてみてる
+                time.sleep(1 * 60 / 2)
+        else:
+            break
