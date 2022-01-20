3267e32d1303416a08ad267f74fc2e8305ca2254
masashinji <masashi.ogino@beproud.jp>
2022-01-20 14:28:39 +0900

    members APIで next_cursor が返ってくるときの考慮を追加

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -30,7 +30,7 @@ class TestCallWhenSlsHaroNotInstalled:
         }
         message = mock.Mock()
         message.body = {
-            "channel": "CCCCCCCCCCC",
+            "channel": "C1234567890",
             "text": "$random",
         }
         with mock.patch("slacker.Conversations.members", return_value=members):
@@ -58,11 +58,55 @@ class TestCallWhenSlsHaroNotInstalled:
         }
         message = mock.Mock()
         message.body = {
-            "channel": "CCCCCCCCCCC",
+            "channel": "C1234567890",
             "text": "$random",
         }
         with mock.patch("slacker.Conversations.members", return_value=members):
             wrapped_func(message)
+
+        # メソッドが呼び出されていない
+        func.assert_not_called()
+
+    def test_when_sls_haro_installed_with_cursor(self, target):
+        """新 haro のユーザーIDが、ページングによる2回目のリクエストで取得されたとき"""
+        func = mock.Mock()
+        wrapped_func = target(func)
+
+        members1 = mock.Mock()
+        members1.body = {
+            "ok": True,
+            "members": [
+                "AAAAAAAAA",
+                "BBBBBBBBB",
+            ],
+            "response_metadata": {
+                "next_cursor": "XXXXXXXXXXXXXXXXXXXX",
+            },
+        }
+        members2 = mock.Mock()
+        members2.body = {
+            "ok": True,
+            "members": [
+                "CCCCCCCCC",
+                "DDDDDDDDD",
+                "U01V2E7MVLG",  # 新 haro のユーザーID
+            ],
+            "response_metadata": {
+                "next_cursor": "",
+            },
+        }
+        message = mock.Mock()
+        message.body = {
+            "channel": "C1234567890",
+            "text": "$random",
+        }
+        with mock.patch("slacker.Conversations.members", side_effect=[members1, members2]) as members_mock:
+            wrapped_func(message)
+
+            # members のAPIが2回呼ばれている
+            assert members_mock.call_count == 2
+            assert members_mock.call_args_list[0] == mock.call("C1234567890", None)
+            assert members_mock.call_args_list[1] == mock.call("C1234567890", "XXXXXXXXXXXXXXXXXXXX")
 
         # メソッドが呼び出されていない
         func.assert_not_called()
