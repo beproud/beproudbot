6df42dad0c6db05e14300d6e88a6c0ed8c51a931
masashinji <masashinji@usernoMacBook-Pro.local>
2022-05-19 15:27:21 +0900

    新haroに移植済の kudo, create, random コマンドに call_when_sls_haro_not_installed デコレータを付加

--- src/haro/plugins/random.py
+++ src/haro/plugins/random.py
@@ -4,7 +4,9 @@ import random
 from slackbot.bot import respond_to
 from slackbot import settings
 import slacker
+
 from haro.botmessage import botsend
+from haro.decorators import call_when_sls_haro_not_installed
 
 
 HELP = '''
@@ -18,6 +20,7 @@ logger = logging.getLogger(__name__)
 
 @respond_to('^random$')
 @respond_to(r'^random\s+(active|help)$')
+@call_when_sls_haro_not_installed
 def random_command(message, subcommand=None):
     """
     チャンネルにいるメンバーからランダムに一人を選んで返す
